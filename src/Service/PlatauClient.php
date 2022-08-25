b0de01f798565e2235c95bc26c0a44cfc00ced08
Kévin <kevindubuc62@gmail.com>
2022-08-25 15:47:20 +0200

    ajout d'une classe PlatauClient fédérant les services platau

---
+++ src/Service/PlatauClient.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Service;
+
+/**
+ * @property PlatauActeur       $acteurs
+ * @property PlatauConsultation $consultations
+ * @property PlatauHealthcheck  $healthcheck
+ * @property PlatauNotification $notifications
+ * @property PlatauPiece        $pieces
+ */
+class PlatauClient extends PlatauAbstract
+{
+    private static array $class_map = [
+        'acteurs'       => PlatauActeur::class,
+        'consultations' => PlatauConsultation::class,
+        'healthcheck'   => PlatauHealthcheck::class,
+        'notifications' => PlatauNotification::class,
+        'pieces'        => PlatauPiece::class,
+    ];
+
+    /**
+     * Initialisation et utilisation d'un service Platau.
+     */
+    public function __get(string $name)
+    {
+        $class_name = \array_key_exists($name, self::$class_map) ? self::$class_map[$name] : null;
+
+        \assert(null !== $class_name, "Service $name inconnu");
+
+        return new $class_name($this->getConfig());
+    }
+}
